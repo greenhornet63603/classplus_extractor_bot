192a5bccb7eac38cb25167ab22b6af102c924653
greenhornet63603 <greenhornet63603@gmail.com>
2025-11-30 01:42:01 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -4,15 +4,13 @@ from pyrogram.types import Message
 
 bot = Client(
     'classplus_bot',
-    api_id=int(os.getenv('27400172')),
-    api_hash=os.getenv('56d0a75c5f9a9de6beb5452aa63c2d36'),
-    bot_token=os.getenv('8204209080:AAHQx2wvrV1yrLoJgrxswYgLBf_mkzXqKHQ')
+    api_id=int(os.getenv('API_ID')),
+    api_hash=os.getenv('API_HASH'),
+    bot_token=os.getenv('BOT_TOKEN')
 )
 
 @bot.on_message(filters.command('start'))
 async def start_cmd(client, message: Message):
     await message.reply_text('👋 Welcome to Classplus Extractor Bot!')
-
-# You would include /drm, /batch, /txt handlers here
 
 bot.run()
